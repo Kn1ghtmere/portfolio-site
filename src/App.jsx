293a69f0d422f6777e69a022325217a89b07529b
subhanali07 <subhanali200823@gmail.com>
2026-09-21 00:07:04 +0500

    starting to work on about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import Home from "./sections/Home"
 import Navbar from "./sections/Navbar"
 import Contact from "./sections/Contact"
+import About from "./sections/About"
 
 function App() {
   return (
     <>
     <Navbar/>
+    <About/>
     <Contact/>
     
     </>
