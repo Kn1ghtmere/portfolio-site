d3fd0f5760785e437b8aacbb64c0afb63dd91456
subhanali07 <subhanali200823@gmail.com>
2026-09-21 02:27:04 +0500

    wrote a readme about current weeks stucture

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Navbar from "./sections/Navbar"
 import Contact from "./sections/Contact"
-import About from "./sections/About"
 
 function App() {
   return (
     <>
     <Navbar/>
-    <About/>
     <Contact/>
     
     </>
